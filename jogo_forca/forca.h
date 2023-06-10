d46840686fa0ca52b57ff4b7e7e3a4e9cb5f46d8
Y4nnLS <101208372+Y4nnLS@users.noreply.github.com>
2023-06-10 20:12:47 -0300

    Upload dos arquivos para o jogo da forca

---
+++ jogo_forca/forca.h
@@ -0,0 +1,12 @@
+#define TAMANHO_PALAVRA 30
+int chuteserrados();
+
+int letraexiste(char letra);
+int enforcou();
+int ganhou();
+void abertura();
+void chuta();
+int jachutou(char letra);
+void desenhaforca();
+void escolhePalavra();
+void adicionaPalavra();
